5ae12ae4b07166ba80b0c644d122952f5ccdbdd3
Marco Pugliese <126952052+Marco-Pugliese@users.noreply.github.com>
2024-02-14 13:02:19 +0100

    complete

--- src/components/LandingPage/ContactMe.jsx
+++ src/components/LandingPage/ContactMe.jsx
@@ -1,28 +1,55 @@
-import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import { Col, Container, Form, InputGroup, Row } from "react-bootstrap";
 import { Envelope, Github, Linkedin } from "react-bootstrap-icons";
-import emailjs from "@emailjs/browser";
-import { useRef } from "react";
+import emailjs, { send } from "@emailjs/browser";
+import { useEffect, useRef, useState } from "react";
 
 const ContactMe = () => {
   const form = useRef();
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [showSend, setShowSend] = useState(false);
+  const [showError, setShowError] = useState(false);
+  // const [clicked, setClicked] = useState(false);
+  const [isEverythingChecked, setIsEverythingChecked] = useState(false);
+  const check = () => {
+    email !== "" && subject !== "" && name !== "" && message.length > 0
+      ? setIsEverythingChecked(true)
+      : setIsEverythingChecked(false);
+  };
+  // const nothing = () => {};
 
+  const sendEmail = () => {
     emailjs
-      .sendForm("service_z2lch6b", "template_8u8vwtl", form.current, {
-        publicKey: "LBJbYc9UBQ2hLfgfJ",
+      .sendForm("service_algs13g", "template_w1nrwer", form.current, {
+        publicKey: "qPPk62aiLtoFATMcB",
       })
       .then(
         () => {
           console.log("SUCCESS!");
+          setShowSend(true);
+          // setClicked(false);
         },
         (error) => {
           console.log("FAILED...", error.text);
+          setShowError(true);
+          // setClicked(false);
         }
       );
   };
+  // useEffect(() => {
+  //   showSend === true && clicked === true
+  //     ? setShowError(false) && setShowSend(false)
+  //     : nothing();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
   return (
-    <Container>
+    <Container
+      onClick={() => {
+        // setClicked(true);
+      }}
+    >
       <div className="py-5 mt-5">
         <div className="width-changing pt-0 mb-0  border-bottom gray-border border-shadow-gray "></div>
         <div className="width-changing pt-0 mb-0 border-bottom gray-border border-shadow-gray rotate-animation-180"></div>
@@ -58,7 +85,12 @@ const ContactMe = () => {
         <div className="width-changing pt-0 mb-0  border-bottom gray-border border-shadow-gray "></div>
         <div className="width-changing pt-0 mb-0 border-bottom gray-border border-shadow-gray rotate-animation-180"></div>
       </div>
-      <form ref={form} onSubmit={sendEmail}>
+      <form
+        ref={form}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <Container>
           <Row className="py-5 mt-5">
             <Col className="col-12 col-lg-6">
@@ -69,11 +101,19 @@ const ContactMe = () => {
                 >
                   Name
                 </InputGroup.Text>
-                <Form.Control
+                <input
+                  type="text"
+                  required
                   name="user_name"
-                  className="bg-transparent light-text"
+                  className="bg-transparent light-text form-control"
                   aria-label="name"
                   aria-describedby="basic-addon1"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                    setShowError(false);
+                    setShowSend(false);
+                  }}
                 />
               </InputGroup>
               <div className="py-2"></div>
@@ -86,11 +126,18 @@ const ContactMe = () => {
                 >
                   eMail
                 </InputGroup.Text>
-                <Form.Control
-                  className="bg-transparent light-text"
+                <input
+                  type="email"
+                  className="bg-transparent light-text form-control"
                   aria-label="email"
                   aria-describedby="basic-addon1"
                   name="user_email"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                    setShowError(false);
+                    setShowSend(false);
+                  }}
                 />
               </InputGroup>
               <div className="py-2"></div>
@@ -103,11 +150,18 @@ const ContactMe = () => {
                 >
                   Subject
                 </InputGroup.Text>
-                <Form.Control
-                  className="bg-transparent light-text"
+                <input
+                  required
+                  className="bg-transparent light-text form-control"
                   aria-label="subject"
                   aria-describedby="basic-addon1"
                   name="user_subject"
+                  value={subject}
+                  onChange={(e) => {
+                    setSubject(e.target.value);
+                    setShowError(false);
+                    setShowSend(false);
+                  }}
                 />
               </InputGroup>
             </Col>
@@ -117,27 +171,69 @@ const ContactMe = () => {
                 <InputGroup.Text className="bg-transparent light-text ">
                   Text
                 </InputGroup.Text>
-                <Form.Control
-                  className="bg-transparent light-text "
+                <textarea
+                  required
+                  className="bg-transparent light-text form-control"
                   as="textarea"
                   aria-label="With textarea"
                   name="message"
+                  value={message}
+                  onChange={(e) => {
+                    setMessage(e.target.value);
+                    setShowError(false);
+                    setShowSend(false);
+                  }}
                 />
               </InputGroup>
+
+              <>
+                {showSend && (
+                  <div className="alert alert-success mt-3" role="alert">
+                    Your message has been sent properly!
+                  </div>
+                )}
+                {showError && (
+                  <div className="alert alert-danger mt-3" role="alert">
+                    Oooops. We had a problem on delivering your message, try
+                    again!
+                  </div>
+                )}
+              </>
             </Col>
             <Col className="col-10 offset-1 py-3">
               <Row className="text-center">
                 <Col className="text-center offset-1 col-4">
                   <button
+                    type="submit"
                     className="w-100 myBtn1 py-1 rounded-3"
-                    onClick={() => sendEmail()}
+                    onClick={(e) => {
+                      e.preventDefault();
+
+                      check();
+                      isEverythingChecked === true
+                        ? sendEmail()
+                        : setShowError(true);
+                      setName("");
+                      setEmail("");
+                      setSubject("");
+                      setMessage("");
+                    }}
                   >
                     Send
                   </button>
                 </Col>
                 <Col className="col-1"></Col>
                 <Col className="text-center offset-1 col-4">
-                  <button className="w-100 myBtn2 py-1 rounded-3">
+                  <button
+                    type="button"
+                    className="w-100 myBtn2 py-1 rounded-3"
+                    onClick={(e) => {
+                      setName("");
+                      setEmail("");
+                      setSubject("");
+                      setMessage("");
+                    }}
+                  >
                     Cancel
                   </button>
                 </Col>
