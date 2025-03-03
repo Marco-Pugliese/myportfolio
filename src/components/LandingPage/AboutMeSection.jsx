2d22e9a692da76e9ef3c7db377b7aab56d6fce28
Marco-Pugliese <marco.pugliese92@yahoo.com>
2025-03-03 21:03:56 +0100

    update

--- src/components/LandingPage/AboutMeSection.jsx
+++ src/components/LandingPage/AboutMeSection.jsx
@@ -2,13 +2,15 @@ import { Container } from "react-bootstrap";
 import WorkPromusic from "./WorkPromusic";
 import WorkBardoo from "./WorkBardoo";
 import WorkNetflics from "./WorkNetflics";
+import InProgress from "./InProgress";
 
 const AboutMeSection = () => {
   return (
     <Container fluid className="p-0 m-0">
-      <WorkPromusic />
+      <InProgress />
+      {/* <WorkPromusic />
       <WorkBardoo />
-      <WorkNetflics />
+      <WorkNetflics /> */}
     </Container>
   );
 };
---
+++ src/components/LandingPage/InProgress.jsx
@@ -0,0 +1,4 @@
+const InProgress = () => {
+  return <div className="w-100 h-100">WORK IN PROGRESS..</div>;
+};
+export default InProgress;
