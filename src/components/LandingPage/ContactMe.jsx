f10808fc69ffcd74f6d2f4b96b2506c851ebb386
Marco-Pugliese <marco.pugliese92@yahoo.com>
2025-03-07 12:37:05 +0100

    upd

--- src/components/LandingPage/ContactMe.jsx
+++ src/components/LandingPage/ContactMe.jsx
@@ -1,14 +1,67 @@
 import { Col, Container, InputGroup, Row } from "react-bootstrap";
-import { Envelope, Github, Linkedin } from "react-bootstrap-icons";
-import { useEffect, useRef, useState } from "react";
+import {
+  Envelope,
+  EnvelopeAtFill,
+  Github,
+  Linkedin,
+  TelephoneFill,
+  Whatsapp,
+} from "react-bootstrap-icons";
 import Icons from "./Icons";
 import ButtonToTop from "./ButtonToTop";
 import InProgress from "./InProgress";
 
 const ContactMe = () => {
+  const phoneNumber = "3382510605";
+  const Callme = "tel:3382510605";
+  const message = "Ciao! Mi piacerebbe sapere di più sui servizi che offri";
+  const whatsapp = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+    message
+  )}`;
+  const email = "pugliesemarco@myyahoo.com";
+  const subjectMail = "Richiesta Informazioni";
+  const bodyMail = "Ciao! Mi piacerebbe sapere di più sui servizi che offri";
+  const mailtoMP = `mailto:${email}?subject=${encodeURIComponent(
+    subjectMail
+  )}&body=${encodeURIComponent(bodyMail)}`;
+
   return (
     <div className="pt-5">
-      <InProgress />
+      <Container>
+        <Row className="my-4 text-center">
+          <a
+            href={mailtoMP}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-light"
+          >
+            <EnvelopeAtFill />
+          </a>
+        </Row>
+        <Row className="my-4 text-center">
+          <a
+            href={Callme}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-light"
+          >
+            <TelephoneFill />
+          </a>
+        </Row>
+        <Row className="my-4 text-center">
+          <a
+            href={whatsapp}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-light"
+          >
+            <Whatsapp />
+          </a>
+        </Row>
+        <Row className="my-4 text-center">
+          <Linkedin />
+        </Row>
+      </Container>
     </div>
   );
 };
